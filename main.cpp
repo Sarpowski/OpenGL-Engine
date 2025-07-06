cefedbdd0f11f913bab9139587b303a97d4da935
Sarpowski <cansarpsakaoglu@gmail.com>
2025-07-07 00:20:36 +0300

    triangle

--- main.cpp
+++ main.cpp
@@ -1,31 +1,54 @@
 #include <iostream>
 #include "GL/glew.h"
-#include "glfw/glfw3.h"
+#include "GLFW/glfw3.h"
 
 int main() {
-
-    GLFWwindow* window;
-    if(!glfwInit()){
+    if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW" << std::endl;
         return -1;
     }
 
-    glewInit();
-    window = glfwCreateWindow(800, 600, "Hello GLFW", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Triangle", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
     glfwMakeContextCurrent(window);
 
-    while(!glfwWindowShouldClose(window)){
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    float positions[6] = {
+            0.0f, 0.5f,   // top center
+            -0.5f,-0.5f, // bottom left
+            0.5f, -0.5f   // bottom right
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (const void*)0);
+
+    while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(0.0f, 0.5f);   // top center
-        glVertex2f(-0.5f, -0.5f); // bottom left
-        glVertex2f(0.5f, -0.5f);  // bottom right
-        glEnd();
-        glfwSwapBuffers(window);
+        //Draw
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDisableVertexAttribArray(0);
     glfwTerminate();
     return 0;
 }
